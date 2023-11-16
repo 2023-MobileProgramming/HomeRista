a4e50f7b2863330be1a2425f4d10e48f2c21b8cf
lth-1026 <th20001026@gmail.com>
2023-11-17 03:28:05 +0900

    BeanSlide 화면을 위한 kotlin 파일 추가

---
+++ app/src/main/java/com/coffee/homerista/BeanSlide/BeanSlidePageFragment.kt
@@ -0,0 +1,27 @@
+package com.coffee.homerista.BeanSlide
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.coffee.homerista.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BeanSlidePageFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class BeanSlidePageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_bean_slide_page, container, false)
+}
