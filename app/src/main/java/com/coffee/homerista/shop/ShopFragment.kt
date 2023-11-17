f6583b78ec1a895cec5960baf230c4fb39a0d06e
ss Kim <25hanayuki@gmail.com>
2023-11-17 13:42:53 +0900

    Fragment updated 1342

--- app/src/main/java/com/coffee/homerista/shop/ShopFragment.kt
+++ app/src/main/java/com/coffee/homerista/shop/ShopFragment.kt
@@ -1,34 +1,57 @@
 package com.coffee.homerista.shop
 
-import android.webkit.WebView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.KeyEvent
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.fragment.app.Fragment
 import com.coffee.homerista.R
-
 
 class ShopFragment : Fragment() {
 
-    private lateinit var webView: WebView
+    companion object {
+        fun newInstance() = ShopFragment()
+    }
+
+    private lateinit var viewModel: ShopViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // fragment_show.xml을 inflate하여 View 객체를 얻습니다.
-        val view = inflater.inflate(R.layout.activity_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_show, container, false)
 
-        // webView를 초기화합니다.
-        webView = view.findViewById(R.id.webView)
+
+
+
+        val webView = view.findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
-
-        // https://shopping.naver.com/home로 웹뷰를 연결합니다.
+        webView.webViewClient = WebViewClient()
         webView.loadUrl("https://search.shopping.naver.com/search/all?query=%EC%BB%A4%ED%94%BC&cat_id=&frm=NVSHATC")
-
+        webView.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    requireActivity().onBackPressed()
+                }
+            }
+            true
+        }
         return view
+
+
+
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ShopViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
 }
