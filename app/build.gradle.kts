09faeaadf2a0de579ea86d950078a4b50ff36634
lth-1026 <th20001026@gmail.com>
2023-11-21 21:07:13 +0900

    viewModel을 사용하기 위한 라이브러리 추가 및 타 라이브러리 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,13 +38,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     //데이터베이스 설정
     val room_version = "2.6.0"
@@ -58,6 +58,10 @@ dependencies {
     implementation("androidx.room:room-guava:$room_version")// optional - Guava support for Room, including Optional and ListenableFuture
     testImplementation("androidx.room:room-testing:$room_version")// optional - Test helpers
     implementation("androidx.room:room-paging:$room_version")// optional - Paging 3 Integration
+
+    //viewModels() 이용하기 위한 라이브러리
+    implementation ("androidx.activity:activity-ktx:1.8.1")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
