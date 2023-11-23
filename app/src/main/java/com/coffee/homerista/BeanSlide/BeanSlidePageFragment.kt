467888cef2689e31b19c72d5158919db23fc22c6
lth-1026 <th20001026@gmail.com>
2023-11-24 03:25:44 +0900

    원두 생성, 수정시 발생하던 오류 해결
    viewPager2는 data가 remove, add 되는 것에 제대로 작동하지 못한다.
    그렇기에 getItemId를 override해 문제를 해결했다.

--- app/src/main/java/com/coffee/homerista/BeanSlide/BeanSlidePageFragment.kt
+++ app/src/main/java/com/coffee/homerista/BeanSlide/BeanSlidePageFragment.kt
@@ -1,6 +1,7 @@
 package com.coffee.homerista.BeanSlide
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.SeekBar
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.coffee.homerista.R
 import com.coffee.homerista.data.entities.Bean
 
@@ -23,6 +25,14 @@ private const val BEAN = "bean"
 
 class BeanSlidePageFragment() : Fragment() {
     private lateinit var bean: Bean
+    private val viewModel: BeanViewModel by activityViewModels {BeanViewModel.Factory}
+    private lateinit var beanName: TextView
+    private lateinit var beanCountries: TextView
+    private lateinit var beanBody: SeekBar
+    private lateinit var beanSweetSalty: SeekBar
+    private lateinit var beanBitterSour: SeekBar
+    private lateinit var beanDarkLight: SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -38,23 +48,24 @@ class BeanSlidePageFragment() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Log.d("page", "페이지 재생성 ${bean}")
+
+        //값이 수정되면 변한 값으로 보여주기 위한 observe 등록
+        viewModel.dataList.observe(viewLifecycleOwner) { dataList ->
+            bean = dataList.find { bean -> bean.id == this.bean.id } ?: bean
+            bindData()
+        }
+
         val content: ConstraintLayout = view.findViewById(R.id.beanPage)
 
-        val beanName: TextView = view.findViewById(R.id.beanName)
-        val beanCountries: TextView = view.findViewById(R.id.beanCountries)
-        val beanBody: SeekBar = view.findViewById(R.id.beanBody)
-        val beanSweetSalty: SeekBar = view.findViewById(R.id.beanSweetSalty)
-        val beanBitterSour: SeekBar = view.findViewById(R.id.beanBitterSour)
-        val beanDarkLight: SeekBar = view.findViewById(R.id.beanDarkLight)
+        beanName = view.findViewById(R.id.beanName)
+        beanCountries = view.findViewById(R.id.beanCountries)
+        beanBody = view.findViewById(R.id.beanBody)
+        beanSweetSalty = view.findViewById(R.id.beanSweetSalty)
+        beanBitterSour = view.findViewById(R.id.beanBitterSour)
+        beanDarkLight = view.findViewById(R.id.beanDarkLight)
 
-        // ViewModel에서 데이터 가져오기
-        beanName.text = bean.name
-        beanCountries.text = bean.countries
-
-        beanBody.progress = bean.body
-        beanSweetSalty.progress = bean.sweetSalty
-        beanBitterSour.progress = bean.bitterSour
-        beanDarkLight.progress = bean.darkLight
+        bindData()
 
         //클릭시 상세페이지로 이동
         content.setOnClickListener {
@@ -68,6 +79,17 @@ class BeanSlidePageFragment() : Fragment() {
                 .commit()
         }
     }
+
+    private fun bindData() {
+        beanName.text = bean.name
+        beanCountries.text = bean.countries
+
+        beanBody.progress = bean.body
+        beanSweetSalty.progress = bean.sweetSalty
+        beanBitterSour.progress = bean.bitterSour
+        beanDarkLight.progress = bean.darkLight
+    }
+
     companion object {
         @JvmStatic
         fun newInstance(bean: Bean) =
